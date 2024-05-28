22467609f91953bc4cd251b1be1ad7f0a7240baf
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-05-28 18:27:15 +0200

    Add HeaderBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const LoggedIn = {firstName: 'Mat'};
   return (
-    <div>Home</div>
+    
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+          type= "greeting"
+          title = "Welcome"
+          user = {LoggedIn?.firstName || 'Guest'}
+          subtext = "Acces and manage your account and transactions efficiently"
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
