597852bbcd571d2f8547bf2ebb9325fe4831eaa8
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-07-08 20:20:22 +0200

    Finished Transfer Payment Page

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,4 +1,5 @@
 import BankCard from '@/components/BankCard';
+import Copy from '@/components/Copy';
 import HeaderBox from '@/components/HeaderBox'
 import { getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
