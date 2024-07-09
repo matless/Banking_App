788d2978eed924b1af3e94da89abb3465361acac
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-07-09 20:16:02 +0200

    Add Pagination

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -4,6 +4,7 @@ import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { formatAmount } from '@/lib/utils';
 import React from 'react'
+import { Pagination } from '@/components/Pagination';
 
 const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
@@ -17,6 +18,14 @@ const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) 
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
   const account = await getAccount({appwriteItemId })
+
+  const rowsPerPage = 10;
+    const totalPages = Math.ceil(account?.transactions.length/rowsPerPage);
+
+    const indexOfLastTransaction = currentPage * rowsPerPage;
+    const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+    const currentTransactions = account?.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction)
   return (
     <div className="transactions">
       <div className="transactions-header">
@@ -52,8 +61,13 @@ const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) 
         
         <section className="flex w full flex-col gap-6">
           <TransactionsTable
-          transactions={account?.transactions}
+          transactions={currentTransactions}
           />
+          {totalPages > 1 && (
+                            <div className="my-4 w-full">
+                                <Pagination totalPages={totalPages} page={currentPage}/>
+                            </div>
+                        )}
         </section>
       </div>
     </div>
